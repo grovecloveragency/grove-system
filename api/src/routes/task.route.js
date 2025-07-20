505c7452a38f6bdff5f64e861c6dc83c52fa0a4b
Saad Sayyed <saadsyed950@gmail.com>
2025-07-20 14:37:36 +0530

    Task API

--- api/src/routes/task.route.js
+++ api/src/routes/task.route.js
@@ -4,5 +4,22 @@ import { Router } from "express";
 const taskRouter = Router();
 
 taskRouter.post("/", taskControllers.createTaskController);
+taskRouter.get(
+  "/active/:usersId",
+  taskControllers.readAllActiveTasksController
+);
+taskRouter.get(
+  "/completed/:usersId",
+  taskControllers.readAllCompletedTasksController
+);
+taskRouter.get(
+  "/failed/:usersId",
+  taskControllers.readAllFailedTasksController
+);
+taskRouter.patch(
+  "/complete/:usersId/:taskId",
+  taskControllers.completeTaskController
+);
+taskRouter.delete("/:usersId/:taskId", taskControllers.deleteTaskController);
 
 export default taskRouter;
--- api/src/services/task.service.js
+++ api/src/services/task.service.js
@@ -33,3 +33,59 @@ export const createTask = async (
 
   return task;
 };
+
+export const readAllActiveTasks = async (usersId) => {
+  return await prisma.tasks.findMany({
+    where: {
+      usersId,
+      status: Status.ACTIVE,
+    },
+  });
+};
+
+export const readAllFailedTasks = async (usersId) => {
+  return await prisma.tasks.findMany({
+    where: {
+      usersId,
+      status: Status.FAILED,
+    },
+  });
+};
+
+export const readAllCompletedTasks = async (usersId) => {
+  return await prisma.tasks.findMany({
+    where: {
+      usersId,
+      status: Status.COMPLETED,
+    },
+  });
+};
+
+export const completeTask = async (usersId, taskId) => {
+  const task = await prisma.tasks.update({
+    where: {
+      id: taskId,
+      usersId,
+    },
+    data: {
+      status: Status.COMPLETED,
+    },
+  });
+
+  await createNotification(`Task completed`, `Has completed the task`, usersId);
+
+  return task;
+};
+
+export const deleteTask = async (usersId, taskId) => {
+  const task = await prisma.tasks.delete({
+    where: {
+      id: taskId,
+      usersId,
+    },
+  });
+
+  await createNotification(`Task deleted`, `Has deleted the task`, usersId);
+
+  return task;
+};
